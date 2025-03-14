6450133dccc517a17f1461e0786b2eecf03d39d3
CodeStone1125 <williechu1125@gmail.com>
2025-03-14 22:03:36 +0800

    Add free block binary search tree in block.h
    Add find_free_tree, find_predecessor_free_tree, remove_free_tree
    , insert_free_tree, new_block, create_tree, print_level_order.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include "../include/memory_allocator.h"
+#include "../include/block.h"
+
 
 int main() {
-    printf("Custom memory allocator test\n");
+    block_t* root = create_tree();
+    print_level_order_with_spacing(root);
+    block_t temp_block;
+    temp_block.size = 5;
+    block_t **node_ptr = find_free_tree(&root, &temp_block);
+    remove_free_tree(&root, new_block(5));
+    node_ptr = find_free_tree(&root, &temp_block);
+    remove_free_tree(&root, new_block(5));
+    printf("\nLevel Order Traversal with Spacing:\n");
+    print_level_order_with_spacing(root);
+    printf("\n");
+
     return 0;
 }
