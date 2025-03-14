86b57adee76118908d40e0f7bf0d6becdd216f01
CodeStone1125 <williechu1125@gmail.com>
2025-03-15 05:17:01 +0800

    Add custom memory allocate
    Modified find target to first fit. Maintaining status
    of allocator and selected free chunk after allocate.
    And return pointer of allocted chunk.
    
    Also delete block.h and modified makefile to use GDB.

--- include/memory_allocator.h
+++ include/memory_allocator.h
@@ -1,3 +1,9 @@
 #include <stddef.h>
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 struct my_allocator  *new_allocator(size_t size);
+char * c_alloc(struct my_allocator **allocator, size_t size);
+void c_free(struct my_allocator **allocator, char *ptr);
